026451c8d048e12867aa8e0da6787ab29e58eabe
Eddy Davila <EdDavila@fullsail.edu>
2013-05-08 20:31:33 -0400

    Added png Center Stage

---
+++ Davila_Eddy_Project_1/js/json.js
@@ -0,0 +1,53 @@
+/* 
+<!-- Eddy Davila -->
+<!-- Project 3 -->
+<!-- VFW 1305 -- >
+*/
+var json = {
+	"contact1" : {
+		"fname": ["First Name:", "Aida"],
+		"lname": ["Last Name:", "Piedra"],
+		"email": ["Email:", "aidapiedra@heaven.com"],
+		"date" : ["Date:", "2013-05-01"],
+		"guitar":["Guitar:", "Ibanez"],
+		"store": ["Store:", "Guitar Center"],
+		"rating":["Rating:", "10"],
+		"pickups":["PickUps:", "EMG"],
+		"strings":["Strings:", "Elixir"],
+		"effects":["Effects:", "Delay"],
+		"curency":["Currency:", "Cash"],
+		"coupon":["Coupon:", "No"],
+		"comments":["Comments:", " Music Forever"]
+	},
+	"contact2" : {
+		"fname": ["First Name:", "Aida"],
+		"lname": ["Last Name:", "Piedra"],
+		"email": ["Email:", "aidapiedra@heaven.com"],
+		"date" : ["Date:", "2013-05-01"],
+		"guitar":["Guitar:", "Ibanez"],
+		"store": ["Store:", "Guitar Center"],
+		"rating":["Rating:", "10"],
+		"pickups":["PickUps:", "EMG"],
+		"strings":["Strings:", "ErnieBall"],
+		"effects":["Effects:", "Distortion"],
+		"curency":["Currency:", "Credit"],
+		"coupon":["Coupon:", "No"],
+		"comments":["Comments:", " Music Forever"]
+	},
+	"contact3" : {
+		"fname": ["First Name:", "Aida"],
+		"lname": ["Last Name:", "Piedra"],
+		"email": ["Email:", "aidapiedra@heaven.com"],
+		"date" : ["Date:", "2013-05-01"],
+		"guitar":["Guitar:", "Ibanez"],
+		"store": ["Store:", "Guitar Center"],
+		"rating":["Rating:", "10"],
+		"pickups":["PickUps:", "DMustain"],
+		"strings":["Strings:", "Elixir"],
+		"effects":["Effects:", "Reverb"],
+		"curency":["Currency:", "Cash"],
+		"coupon":["Coupon:", "No"],
+		"comments":["Comments:", " Music Forever"]
+	}
+
+}
